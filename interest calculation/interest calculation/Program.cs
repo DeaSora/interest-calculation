5a56a69e9af6ebc3af0483b94e3fedfbd8e030e7
DeaSora <ruslanderan@gmail.com>
2022-07-07 16:36:43 +0300

    first step

---
+++ interest calculation/interest calculation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Question2360271
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму вклада");
+            double sum = Convert.ToDouble(Console.ReadLine());
+            // int x = int.Parse(Console.ReadLine());
+
+            if (sum < 10000)
+            {
+                sum += sum * 0.2;
+            }
+            else if (sum <= 100000)
+            {
+                sum += sum * 0.15;
+            }
+            else
+            {
+                sum += sum * 0.1;
+            }
+            Console.WriteLine($"Сумма клада после начисления процентов: {sum}");
+        }
+
+    }
+}
+
+
